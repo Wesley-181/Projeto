228df75bd689e5c3ce638c7ed31ebd9d9f33ba10
Wesley-181 <wesley.m.medeiros@hotmail.com>
2019-03-24 19:36:22 -0300

    Responsivo Finalizado

--- js/efeitos.js
+++ js/efeitos.js
@@ -73,6 +73,8 @@
 
 	});
 
+	
+/* Código do Carousel ATUALIZADO***
 
 	$(document).ready(function(){
 
@@ -94,4 +96,70 @@
 			})
 
 
+	}); */
+
+/* Código do Carousel DEFASADO FUNCIONANDO */
+
+    $(document).ready(function() {
+     
+      $(".thumbnails").owlCarousel({
+     
+          //autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+     
+      });
+     
+    });
+
+/*$(".owl-carousel").owlCarousel()*/
+
+$(document).ready(function() {
+
+	var owl = $(".thumbnails").data('owlCarousel');
+
+	$('#btn-news-prev').on("click", function(){
+
+		owl.prev();
+
 	});
+
+	$('#btn-news-next').on("click", function(){
+
+		owl.next();
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	$("#page-up").on("click", function(event){
+
+		$("body").animate({
+			scrolltTop:0
+		}, 1000);
+
+		//event.preventDefault();
+
+	});
+
+	$("#btn-bars").on("click", function(){
+
+		$("header").toggleClass("open-menu");
+
+	});
+
+	$("#menu-mobile-mask, .btn-close").on("click", function(){
+
+		$("header").removeClass("open-menu");
+
+	});
+
+	$("#btn-search").on("click", function(){
+
+		$("header").toggleClass("open-search");
+		$("input-search-mobile").focus();
+
+	});
+});
